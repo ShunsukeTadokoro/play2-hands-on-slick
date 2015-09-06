7c3e91a37f7a90efa37c1522d769c43bf3ad8f2e
田所 駿佑 <shunsuke.tadokoro@bizreach.co.jp>
2015-09-07 00:15:54 +0900

    add list and edit implementation

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -23,16 +23,37 @@ class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider, val
    * 一覧表示
    */
   def list = Action.async { implicit rs =>
+    // IDの昇順にすべてのユーザ情報を取得
     db.run(Users.sortBy(t => t.id).result).map { users =>
       // 一覧画面を表示
-      Ok(views.html.users.list)
+      Ok(views.html.users.list(users))
     }
   }
 
   /**
    * 編集画面表示
    */
-  def edit(id: Option[Long]) = TODO
+  import UserController._
+  def edit(id: Option[Long]) = Action.async { implicit rs =>
+    // リクエストパラメータにIDが存在する場合
+    val form = if(id.isDefined) {
+      // idからユーザー情報を1件取得
+      db.run(Users.filter(t => t.id === id.get.bind).result.head).map { user =>
+        // 値をフォームに詰める
+        userForm.fill(UserForm(Some(user.id), user.name, user.companyId))
+      }
+    } else {
+      // リクエストパラメータにIDが存在しない場合
+      Future { userForm }
+    }
+
+    form.flatMap {form =>
+      // 会社一覧を取得
+      db.run(Companies.sortBy(_.id).result).map { companies =>
+        Ok(views.html.users.edit(form, companies))
+      }
+    }
+  }
 
   /**
    * 登録実行
@@ -47,6 +68,20 @@ class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider, val
   /**
    * 削除実行
    */
-  def remove = TODO
+  def remove(id: Long) = TODO
 
+}
+
+object UserController {
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // フォームから送信されたデータ⇔ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id"        -> optional(longNumber),
+      "name"      -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
 }
